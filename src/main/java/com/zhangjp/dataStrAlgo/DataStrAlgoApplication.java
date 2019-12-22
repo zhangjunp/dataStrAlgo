92088a288f1745695dead6acb03c62d0051f8d56
zhangjunping <zjp20123258@163.com>
2019-12-22 13:35:01 +0800

    初始化项目

---
+++ src/main/java/com/zhangjp/dataStrAlgo/DataStrAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.zhangjp.dataStrAlgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStrAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStrAlgoApplication.class, args);
+	}
+
+}
